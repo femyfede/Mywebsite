c0e5bfebce3cae90bd269516cdd7dfb152bf82d6
femyfede <156244790+femyfede@users.noreply.github.com>
2025-03-02 03:05:08 -0800

    debug

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -15,16 +15,12 @@ import os
 from django.conf import settings
 
 # Define BASE_DIR
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"), 
 ]
-
 
 
 
@@ -40,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#aviv!ca8_u=utxxoh!!$m71)f52=qs!*w-+v=h&3mpb*s&f13"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
